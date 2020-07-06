a17cea68c044adf0e77bbfcf5374f39ac479beb9
Marco García <marco.garcia@theas.team>
2020-07-05 21:23:19 -0500

    Se crea el proyecto "Aprender POO con PHP" Con definiciones básicas

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>POO</title>
+</head>
+<body>
+    <H1>Programación Orientada a Objetos</H1>
+    <p>Es un paradigma de Programación en el cuál se usan objetos y clases</p>
+
+    <h2>¿Qué es un Objeto?</h2>
+    <p>Son la representacion de elementos donde cada uno tiene un conjunto de atributos que lo hace diferente del resto de objetos</p>
+    <ul>
+    <li>Atributos: caracteristicas que comparte un objeto</li>
+    <li>Métodos: funciones que cumplen un objeto</li>
+    </ul>
+
+    <h2>¿Qué es una Clase?</h2>
+    <p>Es la definición de como se ve un objeto y lo que puede hacer dentro de nuestro codigo <br>
+       Es como una plantilla
+    </p>
+</body>
+</html>
